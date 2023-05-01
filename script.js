85cdcb0c0be8be1872f228ce760afd963d157d2b
ACpanda1408 <103527608+ACpanda1408@users.noreply.github.com>
2023-05-01 10:03:04 +0000

    Onkeydown Javascript debugged :D

---
+++ script.js
@@ -0,0 +1,17 @@
+const button = document.querySelector('button');
+const label = document.querySelector('label');
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+
+function validation() {
+  const emailInput = document.getElementById('email').value;
+  if (emailRegex.test(emailInput)) {
+    label.innerHTML = 'Valid email address';
+    label.style.color = 'green';
+  } else {
+    label.style.color = 'red';
+    label.innerHTML = 'Please provide a valid email address'
+  }
+}
+
+
